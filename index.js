f880f422ce91e70e1802e4c3f0b8d45c3c47ca08
ghosteyetech <sameerat8@gmail.com>
2016-08-12 23:34:25 +0530

    Change index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('App listening on port 3000!');
 });
